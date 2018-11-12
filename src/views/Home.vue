f15970fb3eb97e5d9cff0e7f9cf8072c4acaa828
Jochem Schutte <jochemschutte@Jochems-iMac.local>
2018-11-12 17:41:48 +0100

    begin basic functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }} <button @click="remove(index);">X</button>
+      </li>
+    </ul>
+    <input type="text" v-model="idea" @keyup.enter="submit" />
+
+    <button @click="pick">Pick!</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style>
+input {
+  width: 100%;
+  padding: 0.5rem;
+}
+</style>
 
+<script>
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [],
+      idea: ""
+    };
+  },
+  components: {},
+  methods: {
+    submit() {
+      this.items.push(this.idea);
+      this.idea = "";
+    },
+
+    remove(index) {
+      this.items.splice(index, 1);
+    },
+
+    pick() {
+      let randomIndex = Math.floor(Math.random() * this.items.length);
+
+      alert(this.items[randomIndex]);
+    }
   }
 };
 </script>
